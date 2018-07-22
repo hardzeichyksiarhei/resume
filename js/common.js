b41f81cefbe0f771c14626ae687e2467e35ecba1
hardzeichyksiarhei <hardzeichyksiarhei@gmail.com>
2018-07-22 03:27:48 +0300

    add contact form

--- js/common.js
+++ js/common.js
@@ -19,7 +19,65 @@ jQuery(function ($) {
 			$("#review").owlCarousel({
 					items: 2
 			});
-	}());
+  }());
+  
+	// --------------------------------------------------------------------
+	// Send Form
+  // --------------------------------------------------------------------
+  
+  var $contactForm = $('#feedback-form');
+
+  $contactForm.on('submit', function(e) {
+		e.preventDefault();
+		
+		//get the name field value
+		var name = $('#name').val();
+		//get the name field value
+    var email = $('#email').val();
+    //get the subject field value
+		var subject = $('#subject').val();
+		//get the comments
+		var message = $('#message').val();
+					
+		//pretend we don't need validation
+		
+		//send to formspree
+		$.ajax({
+			url:'https://formspree.io/hardzeichyksiarhei@gmail.com',
+			method: 'POST',
+			data:{
+				name: name,
+				_replyto: email,
+        email: email,
+        message: message,
+				_subject: subject,
+			},
+      dataType: 'json',
+      beforeSend: function() {
+        $contactForm.find('button[type="submit"]').text('Sending...');
+      },
+      success: function() {
+        $contactForm.find('button[type="submit"]').text('Submit');
+        iziToast.success({
+          title: 'Успех...',
+          message: 'Ваше сообщение успешно отправленно',
+          maxWidth: 300,
+          animateInside: false
+        });
+        $contactForm.trigger('reset');
+      },
+      error: function(err) {
+        $contactForm.find('button[type="submit"]').text('Submit');
+        iziToast.error({
+          title: 'Ooops...',
+          message: 'Что-то пошло не так, сообщение не было отправлено. Пожалуйста, попробуйте позже.',
+          maxWidth: 300,
+          animateInside: false
+        });
+      }
+		});		
+		
+	});
 
 
 }); // JQuery end
